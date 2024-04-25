7eb8014057284a180e53d29f0afbfd9ee0d52bf5
Bram van den Heuvel <bram.vdh1999@gmail.com>
2024-04-25 15:34:13 +0200

    Code style fixes

--- everness.lua
+++ everness.lua
@@ -1,13 +1,13 @@
 local water_probability = dynamic_liquid.config.water_probability
 
 if dynamic_liquid.config.water then
-	-- mineral water is already not renewable,
+    -- mineral water is already not renewable,
     -- so it is not necessary to override it
-	local override_def = {liquid_renewable = false}
-	minetest.override_item("everness:mineral_water_source", override_def)
-	minetest.override_item("everness:mineral_water_flowing", override_def)
+    -- local override_def = {liquid_renewable = false}
+    -- minetest.override_item("everness:mineral_water_source", override_def)
+    -- minetest.override_item("everness:mineral_water_flowing", override_def)
 
-	dynamic_liquid.liquid_abm(
+    dynamic_liquid.liquid_abm(
         "everness:mineral_water_source",
         "everness:mineral_water_flowing",
         water_probability
